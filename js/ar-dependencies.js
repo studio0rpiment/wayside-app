5207270b8303e71204accef3521076b2c4cdae7e
eljunkielovegun <eljunkielovegun@users.noreply.github.com>
2025-05-02 03:56:01 +0000

    Deploying to gh-pages from @ eljunkielovegun/wayside-app@30010089cd4032536200bb13dc8172da5bd5e308 🚀

--- js/ar-dependencies.js
+++
@@ -1,2 +0,0 @@
-// This file is intentionally empty - it will be dynamically populated with AR.js and A-Frame scripts
-// through index.html script tags as they need to be loaded in the proper order
