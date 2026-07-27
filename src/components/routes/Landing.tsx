230daefc60585be0b4b294b19ebebebbc3aba2e9
Kevin Patton <9810201+studio0rpiment@users.noreply.github.com>
2026-07-27 16:56:45 +0200

    Update Landing.tsx

--- src/components/routes/Landing.tsx
+++ src/components/routes/Landing.tsx
@@ -74,7 +74,7 @@ const Landing: React.FC = () => {
             <Button {...buttonConfig} />
           </Link>
 
-          <Link to="https://segd.glueup.com/event/segd-dc-chapter-kenilworth-gardens-augmented-reality-experience-183183/" target='_blank' style={{ textDecoration: 'none', border:'1px solid var(--color-light)', borderRadius: '1rem',  "margin": "1rem",  }}>
+          <Link to="https://segd.glueup.com/event/segd-dc-chapter-rescheduled-kenilworth-gardens-ar-experience-188350/" target='_blank' style={{ textDecoration: 'none', border:'1px solid var(--color-light)', borderRadius: '1rem',  "margin": "1rem",  }}>
             <Button {...buttonEvent} />
           </Link>
 
